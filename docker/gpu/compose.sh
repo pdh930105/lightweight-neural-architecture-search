c6845cbb1e539ba0ce89447f94744f5250f2b902
pdh <pdh930105@gmail.com>
2023-03-03 07:15:42 +0000

    update dockerfile

---
+++ docker/gpu/compose.sh
@@ -0,0 +1,19 @@
+export USERID=$(id -u)
+export GROUPID=$(id -g)
+export USERNAME=$(whoami)
+export CONTAINERNAME="lightweight_pdh"
+printf "USERID=%s\n" $USERID 
+printf "GROUPID=%s\n" $GROUPID
+printf "USERNAME=%s\n" $USERNAME 
+xhost +
+xhost +local:docker
+echo $xhost
+#docker-compose up -d --force-recreate --no-deps --build
+docker rm -f ${CONTAINERNAME}
+
+docker-compose up -d --build
+docker exec ${CONTAINERNAME} echo "xhost's display : "
+docker exec ${CONTAINERNAME} echo $DISPLAY
+docker exec ${CONTAINERNAME} echo "visualize test"
+docker exec ${CONTAINERNAME} xclock
+docker exec -it ${CONTAINERNAME} /bin/bash
